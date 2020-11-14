3beadd4743db4793f7f57125f9f078f4c2dea32f
DoraymonIT <bendrimou@gmail.com>
2020-11-14 22:50:39 +0100

    bla bla

--- public/etudiant.php
+++ public/etudiant.php
@@ -1,100 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossorigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <link rel="stylesheet" href="style.css" />
-    <title>
-      Accueil de l'étudiant | Voir le progresse de demande de thèse .
-    </title>
-  </head>
-  <body>
-    <header class="backk">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <img
-              style="float: left"
-              src="assets/img/minstere.png"
-              alt="Ministere LOGO"
-              width="50%"
-            />
-          </div>
-          <div class="col-md-6">
-            <img
-              style="float: right"
-              src="assets/img/FMPM.png"
-              alt="FMPM Logo"
-              width="50%"
-            />
-          </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="style.css" />
+  <title>
+    Accueil de l'étudiant | Voir le progresse de demande de thèse .
+  </title>
+  <style>
+    table,
+    th,
+    td {
+      border: 2px solid teal;
+    text-align: center;
+    padding: 5px;
+    border-collapse: collapse;
+    }
+  </style>
+</head>
+
+<body>
+  <header class="backk">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img style="float: left" src="assets/img/minstere.png" alt="Ministere LOGO" width="50%" />
+        </div>
+        <div class="col-md-6">
+          <img style="float: right" src="assets/img/FMPM.png" alt="FMPM Logo" width="50%" />
         </div>
       </div>
-    </header>
-    <section>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-6">
-            <h5 class="crenau">
-              <i class="fa fa-info-circle" aria-hidden="true"></i> Informations
-              Principales
-            </h5>
-            <br />
-            <b> Votre Numéro Apogée :</b> 123456789 <br />
+    </div>
+  </header>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+          <h5 class="crenau">
+            <i class="fa fa-info-circle" aria-hidden="true"></i> Informations
+            Principales
+          </h5>
+          <br>
+          <b> Votre Numéro Apogée :</b> 123456789 <br />
+          
+          <b> Votre Nom Complet :</b> Ayoub Bendrimou <br />
+         
+          <b> CNE:</b> K123456789 
+         <hr>
+          <b> <u> Accord du Jury </u> <br />
+          <table style="width:100%">
+            <tr>
+              <th>Jury</th>
+              <th>Accord</th>
+            </tr>
+            <tr>
+              <td>L'administration</td>
+              <td><i class="fa fa-check-square" aria-hidden="true"></i> Validé . </td>
+            </tr>
+            <tr>
+              <td>Président de jury</td>
+              <td><i class="fa fa-check-square" aria-hidden="true"></i> Validé .</td>
+            </tr>
+            <tr>
+              <td>L'encadrant ( Rapporteur )</td>
+              <td><i class="fa fa-check-square" aria-hidden="true"></i> Validé .</td>
+            </tr>
+            <tr>
+              <td>Comité du thèse </td>
+              <td><i class="fa fa-check-square" aria-hidden="true"></i> Validé .</td>
+            </tr>
+            <tr>
+              <td>Chef de service</td>
+              <td> <i class="fa fa-spinner" aria-hidden="true"></i> état en cours .</td>
+            </tr>
+          </table>
+        
 
-            <b> Accord de président de jury :</b> <i class="fa fa-spinner" aria-hidden="true"></i> état en cours . <br />
+        <hr />
+          <h5 class="crenau">
+            <i class="fa fa-clock-o" aria-hidden="true"></i> Choix du Créneau
+          </h5>
+          <br />
+          Votre date choisie : <b> 12/07/2020</b> : <b> 12h00 .</b>
+          <br>
+          <a href="index.php"><button class="btn btn-success btn-custom"> <i class="fa fa-pencil" aria-hidden="true"></i> Changer le créneau</button></a>
 
-            <b> Accord de l'encadrant ( Rapporteur ) :</b> <i class="fa fa-check-square" aria-hidden="true"></i> Validé .
-            <br />
-
-            <b> Accord de Chef de service :</b> <i class="fa fa-times" aria-hidden="true"></i> Rejeté .
-            <hr />
-            <h5 class="crenau">
-              <i class="fa fa-clock-o" aria-hidden="true"></i> Choix du Créneau
-            </h5>
-            <br />
-            Votre date choisie : <b> 12/07/2020</b> : <b> 12h00 .</b>
-            <br>
-         <a href="index.php"><button class="btn btn-success btn-custom"> <i class="fa fa-pencil" aria-hidden="true"></i> Changer le créneau</button></a>
-
-          </div>
-          <div class="col-md-3"></div>
         </div>
+        <div class="col-md-3"></div>
       </div>
-    </section>
-    <hr />
-    <footer>
-      <p>
-        Service de scolarité de FMPM
-        <i class="fa fa-copyright" aria-hidden="true"></i> 2020
-      </p>
-    </footer>
-  </body>
-  <script src="assets/js/main.js"></script>
-  <script
-    src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"
-  ></script>
+    </div>
+  </section>
+  <hr />
+  <footer>
+    <p>
+      Service de scolarité de FMPM
+      <i class="fa fa-copyright" aria-hidden="true"></i> 2020
+    </p>
+  </footer>
+</body>
+<script src="assets/js/main.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </html>
