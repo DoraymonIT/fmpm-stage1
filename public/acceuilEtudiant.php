23db9fa4de63caae7ad6995393c153fb7d2b4c2e
DoraymonIT <bendrimou@gmail.com>
2020-11-15 23:42:19 +0100

    just some css staffs

--- public/acceuilEtudiant.php
+++ public/acceuilEtudiant.php
@@ -43,24 +43,25 @@ $result = mysqli_query($db, $query);
             <?php if (isset($_SESSION['CNE'])) : ?>
                 <h6><i class="fa fa-user-circle" aria-hidden="true"></i>
                     Vous êtes Connecte : <?php echo $_SESSION['username'] ?> !</h6>
-                <p><a href="logout.php" class="btn btn-primary" href="#" role="button">
+                <p><a href="logout.php" class="btn btn-primary"  role="button">
                         <i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></p>
             <?php endif ?>
             <hr>
+           
         </div>
         <div class="row">
             <div class="col-md-6 m-auto d-flex flex-column text-center ">
+                <!-- <a href="#">bla bla bla bla bla bla bla bla</a>
                 <a href="#">bla bla bla bla bla bla bla bla</a>
                 <a href="#">bla bla bla bla bla bla bla bla</a>
                 <a href="#">bla bla bla bla bla bla bla bla</a>
-                <a href="#">bla bla bla bla bla bla bla bla</a>
-                <a href="#">bla bla bla bla bla bla bla bla</a>
+                <a href="#">bla bla bla bla bla bla bla bla</a> -->
                 <?php
 
                 if  (mysqli_num_rows($result) == 0) {
-                    echo "<a href='index.php'>demande de soutenance</a>";
+                    echo "<a class='btn btn-outline-dark'  role='button' href='index.php'>Créer une demande de choisir une date de soutenu</a>";
                 }else{
-                    echo "<a href='etudiant.php'>check demande status</a>";
+                    echo "<a class='btn btn-outline-dark'  role='button' href='etudiant.php'>Voir le progresse de demande de thèse</a>";
                 }
 
                 ?>
