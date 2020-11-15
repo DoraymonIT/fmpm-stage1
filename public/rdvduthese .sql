8cc9f570c05e6ecec402605c5c253a3cda73866d
hussein <aitdaoudelhoussein@gmail.com>
2020-11-15 10:51:11 +0100

    gadit les dates bjoj chwya li f index.php + updated db

---
+++ public/rdvduthese .sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : Dim 15 nov. 2020 à 10:45
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `rdvduthese`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `creneau`
+--
+
+CREATE TABLE `creneau` (
+  `id` int(11) NOT NULL,
+  `jour` date NOT NULL,
+  `heure` time NOT NULL,
+  `lieu` varchar(40) NOT NULL,
+  `etat` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `creneau`
+--
+
+INSERT INTO `creneau` (`id`, `jour`, `heure`, `lieu`, `etat`) VALUES
+(1, '2020-11-14', '09:00:00', 'Amphi 1', 1),
+(2, '2020-11-14', '11:00:00', 'Amphi 2', 1),
+(3, '2020-11-29', '09:00:00', 'Amphi 1', 1),
+(4, '2020-11-17', '11:00:00', 'Amphi 2', 1),
+(5, '2020-12-15', '17:02:54', 'Amphi 2', 1),
+(6, '2020-12-16', '15:43:56', 'Amphi 1', 1),
+(7, '2020-12-02', '16:44:26', 'Amphi 6', 1),
+(8, '2020-12-02', '14:00:00', 'Amphi 4', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+CREATE TABLE `etudiant` (
+  `CNE` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_apoge` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `etudiant`
+--
+
+INSERT INTO `etudiant` (`CNE`, `email`, `no_apoge`) VALUES
+('G145625311', 'aitdaoudelhoussein@gmail.com', 1234567),
+('R32656', 'email@gmail.com', 554666);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prof`
+--
+
+CREATE TABLE `prof` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `prenom` varchar(40) NOT NULL,
+  `noProf` varchar(40) NOT NULL,
+  `spec` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `prof`
+--
+
+INSERT INTO `prof` (`id`, `nom`, `prenom`, `noProf`, `spec`, `email`) VALUES
+(1, 'Jalal', 'Hamid', '32456710', 'Pediatrie', 'jalal.hamid@gmail.com'),
+(2, 'Edrissi', 'Khadija', '98673517', 'Orthophonie', 'edrissi.khadeja@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `soutenace`
+--
+
+CREATE TABLE `soutenace` (
+  `soutenance_id` int(11) NOT NULL,
+  `date_depot_sujet` date NOT NULL,
+  `directeur` int(11) NOT NULL,
+  `intitule_these` varchar(126) NOT NULL,
+  `nature_these` varchar(126) NOT NULL,
+  `materiel_d_etude_et_echantillioannage` varchar(255) NOT NULL,
+  `duree_d_etude` time NOT NULL,
+  `lieu_d_etude` varchar(255) NOT NULL,
+  `objectif_d_etude` varchar(1024) NOT NULL,
+  `mots_cles` varchar(1024) NOT NULL,
+  `president` int(11) NOT NULL,
+  `jury1` int(11) NOT NULL,
+  `jury2` int(11) NOT NULL,
+  `jury3` int(11) NOT NULL,
+  `jury4` int(11) DEFAULT NULL,
+  `etudiant` varchar(15) NOT NULL,
+  `creneau` int(11) NOT NULL,
+  `etat` tinyint(4) NOT NULL,
+  `motif` varchar(600) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `these`
+--
+
+CREATE TABLE `these` (
+  `id` int(11) NOT NULL,
+  `intitule` varchar(40) NOT NULL,
+  `id_directeur_these` int(100) NOT NULL,
+  `nature_etude` varchar(100) NOT NULL,
+  `materiel_etude_echan` varchar(100) NOT NULL,
+  `duree_etude` varchar(100) NOT NULL,
+  `lieu_etude` varchar(100) NOT NULL,
+  `objectifes_etude` text NOT NULL,
+  `mots_cles` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `these`
+--
+
+INSERT INTO `these` (`id`, `intitule`, `id_directeur_these`, `nature_etude`, `materiel_etude_echan`, `duree_etude`, `lieu_etude`, `objectifes_etude`, `mots_cles`) VALUES
+(1, 'These 1', 2, 'nkldsnfksndknsdz', 'ansmdknasdklncklsndacksdzn', 'aksdjkasd', 'kasndknaskdns', 'kasnfkansdfknaskfnaksfnaksnf', 'lakdksa,asdojas,asofj,aoskjf,asofj');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `creneau`
+--
+ALTER TABLE `creneau`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  ADD PRIMARY KEY (`CNE`),
+  ADD UNIQUE KEY `CNE` (`CNE`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `prof`
+--
+ALTER TABLE `prof`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `soutenace`
+--
+ALTER TABLE `soutenace`
+  ADD PRIMARY KEY (`soutenance_id`),
+  ADD KEY `directeur` (`directeur`),
+  ADD KEY `president` (`president`),
+  ADD KEY `jury1` (`jury1`),
+  ADD KEY `jury2` (`jury2`),
+  ADD KEY `jury3` (`jury3`),
+  ADD KEY `jury4` (`jury4`),
+  ADD KEY `etudiant` (`etudiant`),
+  ADD KEY `creneau` (`creneau`);
+
+--
+-- Index pour la table `these`
+--
+ALTER TABLE `these`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_prof_these` (`id_directeur_these`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `creneau`
+--
+ALTER TABLE `creneau`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `prof`
+--
+ALTER TABLE `prof`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `soutenace`
+--
+ALTER TABLE `soutenace`
+  MODIFY `soutenance_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `these`
+--
+ALTER TABLE `these`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `soutenace`
+--
+ALTER TABLE `soutenace`
+  ADD CONSTRAINT `soutenace_ibfk_1` FOREIGN KEY (`directeur`) REFERENCES `prof` (`id`),
+  ADD CONSTRAINT `soutenace_ibfk_2` FOREIGN KEY (`president`) REFERENCES `prof` (`id`),
+  ADD CONSTRAINT `soutenace_ibfk_3` FOREIGN KEY (`jury1`) REFERENCES `prof` (`id`),
+  ADD CONSTRAINT `soutenace_ibfk_4` FOREIGN KEY (`jury2`) REFERENCES `prof` (`id`),
+  ADD CONSTRAINT `soutenace_ibfk_5` FOREIGN KEY (`jury3`) REFERENCES `prof` (`id`),
+  ADD CONSTRAINT `soutenace_ibfk_6` FOREIGN KEY (`jury4`) REFERENCES `prof` (`id`),
+  ADD CONSTRAINT `soutenace_ibfk_7` FOREIGN KEY (`etudiant`) REFERENCES `etudiant` (`CNE`),
+  ADD CONSTRAINT `soutenace_ibfk_8` FOREIGN KEY (`creneau`) REFERENCES `creneau` (`id`);
+
+--
+-- Contraintes pour la table `these`
+--
+ALTER TABLE `these`
+  ADD CONSTRAINT `id_prof_these` FOREIGN KEY (`id_directeur_these`) REFERENCES `prof` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- public/test.php
+++ public/test.php
@@ -1,84 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="pickadate.js-3.6.2/lib/themes/default.css">
-    <link rel="stylesheet" href="pickadate.js-3.6.2/lib/themes/default.date.css">
-    <link rel="stylesheet" href="pickadate.js-3.6.2/lib/themes/default.time.css">
-  </head>
-  <body>
-    <input
-      id="date1"
-      size="60"
-      type="date"
-      format="DD/MM/YYYY"
-      placeholder="DD/MM/YYYY"
-      min="2020-07-01"
-      max="2020-10-30"
-    />
-    <input class="datepicker" type="date" name="" id=""  />
-    <br />
-    <hr />
-  </body>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<?php
+require_once("database_connect.php");
+$max_date = $_POST["max-date"];
+$min_date = $_POST["min-date"];
+if (!empty($max_date) && !empty($min_date)) {
+    $date_arr=array();
 
- <script src="pickadate.js-3.6.2/lib/picker.js"></script>
- <script src="pickadate.js-3.6.2/lib/picker.date.js"></script>
- <script src="pickadate.js-3.6.2/lib/picker.time.js"></script>
-  <script>
-var $input = $('.datepicker').pickadate();
+    $fmax_date= date_format(new DateTime($max_date), 'Y-m-d');
+    $fmin_date= date_format(new DateTime($min_date), 'Y-m-d');
+    $query = "SELECT * FROM creneau WHERE jour between '$fmin_date' and '$fmax_date' ";
+    $result = mysqli_query($db, $query);
+    if  (mysqli_num_rows($result) != 0) {
+        foreach ($result as $c) {
+            $date_arr[]=$c['jour'];
+        }
+        echo json_encode($date_arr);
+    }
+}
 
-// Use the picker object directly.
-var picker = $input.pickadate('picker');
-// Disable all the dates
-picker.set('disable', [
-  { from: [2020,3,14], to: [2020,3,21] }
-])
-
-
-  </script>
-  <script>
-  var $input = $('.datepicker').pickadate({
-    onClose: function() {
-      console.log(this.get('select', 'yyyy/mm/dd'));
-      // console.log("ha ana ");
-      $("#creneux").fadeIn(2000);
-      // }
-    },
-    // onSet: function(context) {
-    //   // console.log("select"+ this.get('select'));
-    //   foo = new Date(context.select);
-    //       var curr_date = foo.getDate();
-
-    //       var curr_month = foo.getMonth();
-
-    //       var curr_year = foo.getFullYear();
-    //       var dateFormated=(curr_date + "-" + curr_month + "-" + curr_year);
-    //       console.log(dateFormated);
-    //       if(dateFormated==null){
-    //         document.getElementById("creneux").style.display="none";
-    //       }
-    // }
-  });
-
-  // Use the picker object directly.
-  var picker = $input.pickadate('picker');
-  // Disable all the dates
-  if (picker.clear()) {
-    $("#creneux").fadeOut(1000);
-  }
-  picker.set('disable', [{
-    from: [2020, 10, 14],
-    to: [2020, 10, 21]
-  }]);
-  //  var x = document.getElementsByClassName("picker__box"); 
-  //  var x = $(".picker__box");
-  //  x.on('click', function() {
-  //    console.log("select"+ picker.get('select'));
-  //   console.log("date"+ picker.get('select', 'yyyy/mm/dd'));
-  //     document.getElementById("creneux").style.display="block";
-  //   });
-</script>
-</html>
