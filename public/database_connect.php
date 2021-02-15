f34e285d36e7e41e4999063f01834bc453d76016
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-02-15 13:33:53 +0100

    db

--- public/database_connect.php
+++ public/database_connect.php
@@ -1,6 +1,6 @@
 <?php
  //$db = mysqli_connect('localhost', 'root', '', 'id15444825_rd','3306');
- $db = mysqli_connect('localhost', 'root', '', 'rd_new','3306');
+ $db = mysqli_connect('localhost', 'root', '', 'rd','3306');
 
  if(!$db) {
      die('Connection failed: ' . mysqli_connect_error());
